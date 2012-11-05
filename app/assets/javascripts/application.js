151da98319167a44ab88de6dce7464d50b373b1f
Joël Gähwiler <joel.gaehwiler@bluewin.ch>
2012-11-06 00:16:04 +0100

    100% height and cmd+e & cmd+s hotkeys

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,5 @@
+//= require jquery
+//= require jquery_ujs
 //= require_self
 //= require_tree .
 //= require codemirror
@@ -6,3 +8,13 @@
 //= require codemirror/modes/markdown
 //= require codemirror/modes/ruby
 //= require codemirror/modes/javascript
+
+$(document).ready(function(){
+  $(window).resize(doResize);
+  doResize();
+});
+
+function doResize() {
+  $("body, .CodeMirror").height(0);
+  $("body, .CodeMirror").height($(document).height());
+}
