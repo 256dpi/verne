31147261bf33b68adac66c53758606d4f4aa3fe4
Joël Gähwiler <joel.gaehwiler@bluewin.ch>
2012-11-06 00:15:30 +0100

    check for session expiration

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -1,31 +1,43 @@
 class ProjectController < ApplicationController
   
   def view
-    @file = params[:file]+".md"
-    @file_full = session[:path]+"/"+@file
-    if not File.file? @file_full
-      redirect_to "/project/edit/#{params[:file]}"
+    if session.has_key? :path
+      @file = params[:file]+".md"
+      @file_full = session[:path]+"/"+@file
+      if not File.file? @file_full
+        redirect_to "/project/edit/#{params[:file]}"
+      end
+    else
+      redirect_to root_url
     end
   end
   
   def edit
-    @file = params[:file]
-    @file_full = session[:path]+"/"+@file+".md"
-    if File.file? @file_full
-      @content = File.read(@file_full)
+    if session.has_key? :path
+      @file = params[:file]
+      @file_full = session[:path]+"/"+@file+".md"
+      if File.file? @file_full
+        @content = File.read(@file_full)
+      else
+        @content = ""
+      end
     else
-      @content = ""
+      redirect_to root_url
     end
   end
   
   def save
-    @file = params[:file]+".md"
-    @file_full = session[:path]+"/"+@file
-    @code = params[:code] || ""
-    @code = @code.gsub("\t","  ")
-    `mkdir -p #{File.dirname(@file_full)}`
-    File.open(@file_full,"w") {|f| f.write(@code) }
-    redirect_to "/project/view/#{params[:file]}"
+    if session.has_key? :path
+      @file = params[:file]+".md"
+      @file_full = session[:path]+"/"+@file
+      @code = params[:code] || ""
+      @code = @code.gsub("\t","  ")
+      `mkdir -p #{File.dirname(@file_full)}`
+      File.open(@file_full,"w") {|f| f.write(@code) }
+      redirect_to "/project/view/#{params[:file]}"
+    else
+      redirect_to root_url
+    end
   end
   
   def open
--- app/controllers/start_controller.rb
+++ app/controllers/start_controller.rb
@@ -1,6 +1,7 @@
 class StartController < ApplicationController
   
   def index
+    @old_path = session[:path] || ""
   end
 
 end
