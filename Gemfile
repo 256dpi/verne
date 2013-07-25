0bc65f3c63f06d0c486ebd99f4c0f6d48dc091fb
Pratik Mukerji <pratik@electricfeel.com>
2013-07-25 09:53:01 +0200

    updated to rails 4

--- Gemfile
+++ Gemfile
@@ -1,15 +1,12 @@
 source "https://rubygems.org"
 
-gem "rails", "3.2.7"
+gem "rails", "4.0.0"
+gem "activeresource"
 gem "sqlite3"
-
-group :assets do
-  gem "sass-rails", "~> 3.2.3"
-  gem "coffee-rails", "~> 3.2.1"
-  gem "therubyracer", :platforms => :ruby
-  gem "uglifier", ">= 1.0.3"
-end
-
+gem "sass-rails"
+gem "coffee-rails"
+gem "therubyracer", :platforms => :ruby
+gem "uglifier"
 gem "jquery-rails"
 gem "haml"
 gem "codemirror-rails"
--- config/application.rb
+++ config/application.rb
@@ -5,12 +5,10 @@ require "action_mailer/railtie"
 require "active_resource/railtie"
 require "rails/test_unit/railtie"
 require "sprockets/railtie"
+require "makeup/markup"
 
 if defined?(Bundler)
-  # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
-  # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
+  Bundler.require(:default, Rails.env)
 end
 
 module Verne
